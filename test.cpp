7280545d2ac2c2fedd27c90a28291255d8e63a62
baiyeshishen <tyutgaoze@163.com>
2020-06-20 10:36:28 +0800

    第一次提交

---
+++ test.cpp
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-20 10:32:59
+ * @LastEditTime: 2020-06-20 10:34:02
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \test\test.cpp
+ */ 
+#include<iostream>
+int main(){
+    std::cout<<"haha"<<endl;
+    return 0;
+}
