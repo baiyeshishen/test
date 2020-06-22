2598d99ed25729010d51c9006048150a7c8fbd0b
baiyeshishen <tyutgaoze@163.com>
2020-06-22 17:15:48 +0800

    选择排序

---
+++ selectionSort.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+template<typename T>
+void selectionSort(T arr[],int n){
+     for(int i=0;i<n;i++){
+             int minIndex=i;
+	     for(int j=i+1;j<n;j++){
+                 if(arr[j]<arr[minIndex])
+			 minIndex=j;
+	     }
+	     sawp(arr[minIndex],arr[i]);
+     }
+     return ;
+}
